94855ea6b1485aa1c6f4ee9eeff558a7c82cb982
shougarianh <shougarianh@gmail.com>
2021-12-15 13:15:01 -0500

    create account activity

--- app/src/main/java/com/example/cc443_final_project/MainActivity.java
+++ app/src/main/java/com/example/cc443_final_project/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.cc443_final_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    private Button createAccount;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
+        createAccount = (Button) findViewById(R.id.create_account_button);
+        createAccount.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.create_account_button:
+                startActivity(new Intent(this, createAccount.class));
+                break;
+        }
     }
 }
+
---
+++ app/src/main/java/com/example/cc443_final_project/createAccount.java
@@ -0,0 +1,21 @@
+package com.example.cc443_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import com.google.firebase.auth.FirebaseAuth;
+
+public class createAccount extends AppCompatActivity {
+    //private FirebaseAuth mAuth; // part of checking current Auth state
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+        //mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void registerUser() {
+        String email = "TODO" ; // convert to string using edit.getText().
+    }
+}
