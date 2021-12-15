692ae993ab73ddf32691253f6961e025175c0d28
Nikol Vladinska <nikolvladinska@MacBook-Pro-14-inch-2021.local>
2021-12-15 17:10:28 -0500

    Beginning functionality on adding a new workout

--- app/src/main/java/com/example/cc443_final_project/homePage.java
+++ app/src/main/java/com/example/cc443_final_project/homePage.java
@@ -1,15 +1,25 @@
 package com.example.cc443_final_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class homePage extends AppCompatActivity {
+public class homePage extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+    }
+
+    public void onClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.add_workout:
+                startActivity(new Intent(this, addWorkout.class));
+        }
     }
 }
