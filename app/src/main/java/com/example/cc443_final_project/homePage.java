1f93059760af7ec57317fb0a1491591047e085e8
Nikol Vladinska <nikolvladinska@MacBook-Pro-14-inch-2021.local>
2021-12-15 17:43:31 -0500

    Nav bar functionality

--- app/src/main/java/com/example/cc443_final_project/homePage.java
+++ app/src/main/java/com/example/cc443_final_project/homePage.java
@@ -3,23 +3,54 @@ package com.example.cc443_final_project;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class homePage extends Activity implements View.OnClickListener {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class homePage extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    Button addWorkoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addWorkoutButton = findViewById(R.id.add_workout);
+        addWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(homePage.this, addWorkout.class));
+            }
+        });
     }
 
-    public void onClick(View v)
-    {
-        switch (v.getId()) {
-            case R.id.add_workout:
-                startActivity(new Intent(this, addWorkout.class));
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
+
 }
