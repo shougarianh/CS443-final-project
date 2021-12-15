6a67cad745cf6b7bd75a227a9c359f390ee628a6
shougarianh <shougarianh@gmail.com>
2021-12-15 15:54:27 -0500

    added registration

--- app/src/main/java/com/example/cc443_final_project/createAccount.java
+++ app/src/main/java/com/example/cc443_final_project/createAccount.java
@@ -1,14 +1,22 @@
 package com.example.cc443_final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -69,10 +77,46 @@ public class createAccount extends AppCompatActivity implements View.OnClickList
         {
             editTextPassword.setError("Password must contain atleast 8 characters and one " +
                     "special character");
+            return;
         }
-        if (reEnterPassword.isEmpty() || reEnterPassword != password)
+        if (reEnterPassword.isEmpty() || !reEnterPassword.equals(password))
         {
             editTextReEnterPassword.setError("Passwords must match!");
+            return;
         }
+        final boolean[] loggedIn = {false};
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            /*
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue("HELLO").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(createAccount.this, "Succesfully registered!", Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        Toast.makeText(createAccount.this, "Failed to register!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                             */
+
+                        }
+                        else {
+
+                            Toast.makeText(createAccount.this, "Failed to register!", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+        });
     }
 }
