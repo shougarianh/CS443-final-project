6355a8c41e6dfb383479302d91b9bf059930ef9f
shougarianh <shougarianh@gmail.com>
2021-12-15 18:41:59 -0500

    logout is done

--- app/src/main/java/com/example/cc443_final_project/homePage.java
+++ app/src/main/java/com/example/cc443_final_project/homePage.java
@@ -3,6 +3,7 @@ package com.example.cc443_final_project;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +13,19 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class homePage extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
     Button addWorkoutButton;
+    MenuItem logoutButton;
+    NavigationView navigationView;
+    FirebaseAuth myAuth;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +45,43 @@ public class homePage extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         addWorkoutButton = findViewById(R.id.add_workout);
+        // Set up the navigation view
+        navigationView = (NavigationView) findViewById(R.id.navmenu);
+        logoutButton = navigationView.getMenu().findItem(R.id.nav_logout);
+        myAuth = FirebaseAuth.getInstance();
+
+        logoutButton.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                myAuth.signOut();
+                startActivity(new Intent(homePage.this, createAccount.class));
+                return true;
+            }
+        });
+
+        myAuth = FirebaseAuth.getInstance();
+
+
+        /*
         addWorkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(homePage.this, addWorkout.class));
             }
         });
+
+         */
+
+        //// DATABASE TESTING/////////
+
+
+        /////////////////////////////
+
+
+
+
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
