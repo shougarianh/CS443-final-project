74711da3c28bc3b101649caad83c403b269a5740
shougarianh <shougarianh@gmail.com>
2021-12-17 15:20:12 -0500

    data on the server is now displayed in list view localy

--- app/src/main/java/com/example/cc443_final_project/Workout.java
+++ app/src/main/java/com/example/cc443_final_project/Workout.java
@@ -2,10 +2,30 @@ package com.example.cc443_final_project;
 public class Workout {
     public String type, length, calories, dateString;
 
+    public Workout() {
+
+    }
+
     public Workout(String type, String length, String calories, String dateString) {
         this.type = type;
         this.length = length;
         this.calories = calories;
         this.dateString = dateString;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+    public String getLength()
+    {
+        return length;
+    }
+    public String getCalories(){
+        return calories;
+    }
+    public String getDateString()
+    {
+        return dateString;
     }
 }
