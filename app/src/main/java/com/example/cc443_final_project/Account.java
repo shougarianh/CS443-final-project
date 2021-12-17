68c5ce3172296da33c78adf2713fcf1e3a4f2c00
shougarianh <shougarianh@gmail.com>
2021-12-17 17:13:00 -0500

    fixed some bugs

--- app/src/main/java/com/example/cc443_final_project/Account.java
+++ app/src/main/java/com/example/cc443_final_project/Account.java
@@ -3,12 +3,16 @@ package com.example.cc443_final_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Account extends AppCompatActivity {
+
+    private Button resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,15 @@ public class Account extends AppCompatActivity {
         if (user != null) {
             email.setText(user.getEmail());
         }
+
+        resetPassword = (Button) findViewById(R.id.change_password);
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo
+            }
+        });
 
     }
 }
