4dd9ebb08a597b6af033ffe40263b97c4c2b2668
Nikol Vladinska <nikolvladinska@MacBook-Pro-14-inch-2021.local>
2021-12-18 11:09:29 -0500

    Adding reset password functionality + some UI fixes

--- app/src/main/java/com/example/cc443_final_project/Account.java
+++ app/src/main/java/com/example/cc443_final_project/Account.java
@@ -1,18 +1,26 @@
 package com.example.cc443_final_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class Account extends AppCompatActivity {
 
     private Button resetPassword;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,7 @@ public class Account extends AppCompatActivity {
         TextView email = findViewById(R.id.current_user_view);
 
         // https://stackoverflow.com/questions/54784101/firebase-android-get-current-user-email
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             email.setText(user.getEmail());
         }
@@ -33,9 +41,28 @@ public class Account extends AppCompatActivity {
         resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // todo
+                resetPassword();
             }
         });
 
+    }
+
+    private void resetPassword() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String email_address = Objects.requireNonNull(user.getEmail()).trim();
+        auth.sendPasswordResetEmail(email_address)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Account.this, "We have sent you " +
+                                    "instructions to reset your password!",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Account.this, "Failed to send reset " +
+                                    "email!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
